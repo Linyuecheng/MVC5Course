e78fc9d496009e496e9811f51cf43ec5de8cb76e
Cheng <cheng@gittest.com>
2016-10-30 09:56:52 +0800

    練習 [HandleError] 自訂例外呈現畫面

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,12 +1,14 @@
 ﻿using MVC5Course.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+    [HandleError(ExceptionType = typeof(DbEntityValidationException), View = "Error_DbEntityValidationException")]
     public class MBController : BaseController
     {
         // GET: MB
